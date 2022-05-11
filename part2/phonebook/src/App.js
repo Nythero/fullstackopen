6de21b15260eb341d8090cb4f8dce4b569450b10
Agustin Gonzalez <agustin-ag@live.com>
2022-05-11 09:38:50 +0300

    Added exercise 2.8 Phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,31 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button type="submit" onClick={onClick}> {text}</button>
 
-const Person = ({ person }) => <p>{person.name}</p>
+const Person = ({ person }) => <p>{person.name} {person.phone}</p>
 
 const Persons = ({ persons }) => persons.map(
   (person) => <Person person={person} key={person.name} />)
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '123-456-789' }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
-  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleValueChange = (stateSetter) => (event) => stateSetter(event.target.value)
+
+  const setNewPhoneNumber = (value) => {
+    const regex = /^[\d-]*$/
+    if(regex.test(value)){
+      setNewPhone(value)
+    }
+  }
 
   const handleClick = (event) => {
     event.preventDefault()
 
-    if(newName === '') 
+    if(newName === '' || newPhone === '') 
       return 0
     else if(persons.find((e) => e.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
@@ -26,12 +34,13 @@ const App = () => {
     }
 
     const newPersons = [...persons]
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, phone: newPhone }
 
     newPersons.push(newPerson)
     
     setPersons(newPersons)
     setNewName('')
+    setNewPhone('')
   }
 
   return (
@@ -39,7 +48,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newName} onChange={handleValueChange(setNewName)} />
+	  phone number: <input 
+	    value={newPhone}
+	    onChange={handleValueChange(setNewPhoneNumber)} />
         </div>
         <div>
           <Button onClick={handleClick} text='add' />
