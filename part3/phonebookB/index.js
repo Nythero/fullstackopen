9b538d7dec3dfd18f56aee1f240efa60c5e597dd
Agustin Gonzalez <agustin-ag@live.com>
2022-05-17 07:23:57 +0300

    Added exercise 3.4 Phonebook Backend Step4

--- part3/phonebookB/index.js
+++ part3/phonebookB/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = require('./db.json')
+let persons = require('./db.json')
 
 app.get('/api/persons', (req, res) => {
   res.status(200).json(persons)
@@ -16,6 +16,12 @@ app.get('/api/persons/:id', (req, res) => {
   else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
