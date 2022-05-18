e489d13936dd22e0c0816d9daf7cf06141905b26
Agustin Gonzalez <agustin-ag@live.com>
2022-05-19 02:33:23 +0300

    Added exercise 3.18 Phonebook Database Step6

--- part3/phonebookB/src/routes/info.js
+++ part3/phonebookB/src/routes/info.js
@@ -1,15 +1,23 @@
-const info = (req, res) => {
-  const html = `<html>
-      <body>
-        <p>
-	  Phonebook has info for ${persons.length}
-	</p>
-	<p>
-	  ${new Date()}
-	</p>
-      </body>
-    </html>`
-  res.status(200).send(html)
+const Person = require('../models/Person.js')
+
+const info = async (req, res, next) => {
+  try {
+    const people = await Person.find({})
+    const html = `<html>
+        <body>
+          <p>
+            Phonebook has info for ${people.length}
+          </p>
+          <p>
+            ${new Date()}
+          </p>
+        </body>
+      </html>`
+    res.status(200).send(html)
+  }
+  catch(err) {
+    next(err)  
+  }
 }
 
 module.exports = info
