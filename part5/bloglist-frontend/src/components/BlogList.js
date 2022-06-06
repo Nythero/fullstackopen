6d3308153fdf3e19ef5d61e1a7bf8404a0a02dfe
Agustin Gonzalez <agustin-ag@live.com>
2022-06-06 19:36:39 -0300

    Added Exercise 5.9 Bloglist Frontend Step9

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -4,9 +4,19 @@ const blogComponent = blogsState => blog => {
   return <Blog key={blog.id} blog={blog} blogsState={blogsState}/>
 }
 
+const sortingBlogsFunction = (b1, b2) => {
+  if(b1.likes > b2.likes)
+    return -1
+  else if (b2.likes > b1.likes)
+    return 1
+  else
+    return 0
+}
+
 const BlogList = ({ blogsState }) => {
   const [blogs] = blogsState
-  return blogs.map(blogComponent(blogsState))
+  const sortedBlogs = blogs.sort(sortingBlogsFunction)
+  return sortedBlogs.map(blogComponent(blogsState))
 }
 
 export default BlogList
