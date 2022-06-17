2adf2f483ccf7caa7c22337d844f4122dcb6b00b
Agustin Gonzalez <agustin-ag@live.com>
2022-06-17 09:01:04 +0300

    Added exercise 6.5 Anecdotes Step3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,6 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { vote, newAnecdote } from './reducers/anecdoteReducer.js'
 
+const isMoreVoted = (a1, a2) => a1.votes > a2.votes
+
+const sortingFunction = (a1, a2) => {
+  if(isMoreVoted(a1, a2))
+    return -1
+  else if(isMoreVoted(a2, a1))
+    return 1
+  else 
+    return 0
+}
+
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
@@ -15,7 +26,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {[...anecdotes].sort(sortingFunction).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
